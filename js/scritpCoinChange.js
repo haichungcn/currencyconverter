fe89bd1208ada2941dc2b10633a950204b042222
haichungcn <haichungcn@gmail.com>
2019-10-02 00:22:54 +0700

    Finished Coin Change

--- js/scritpCoinChange.js
+++ js/scritpCoinChange.js
@@ -12,10 +12,6 @@
 
 const listOfBills = [500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000];
 let amountOfBills = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-// let objectOfBills = {
-//   500000 : 
-
-// }
 
 let database = {
   500000 : [1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,74 +25,67 @@ let database = {
   1000 : [0, 0, 0, 0, 0, 0, 0, 0, 1]
 };
 
-const amntToChange = 597000;
+let newAmount = 0;
 
 function changeCal(amnt) {
     let amountOfBills = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let newAmount = amnt;
-
+    newAmount = amnt;
     // checking which key in the database is the closest to amnt, if there is one,
     // copy its values to amountOfBills,
     // reduce amnt by that number and continue with the newAmount.
     
     let listKeysDatabase = Object.keys(database);
-    listKeysDatabase.reverse()
-    var found = listKeysDatabase.find(function(element) {
+    listKeysDatabase.sort(function(a, b){return b-a});
+    let found = listKeysDatabase.find(function(element) {
       return element <= amnt;
     });
 
     amountOfBills = database[found];
-    console.log("found is", found, "and amountOfBills is", amountOfBills);
+    // console.log("found is", found, "and amountOfBills is", amountOfBills);
     newAmount = amnt - parseInt(found);
-    console.log("newAmount is ", newAmount);
+    // console.log("newAmount is ", newAmount,"the database-500k is ", database["500000"]);
 
+    let newAmountOfBills = [];
+    amountOfBills.forEach(function(item){
+      newAmountOfBills.push(item); 
+    })
 
     for (let i = 0; i < listOfBills.length; i++) {
-        console.log("recursion of i", i, "and amount is ", newAmount);
+        // console.log("recursion of i", i, "and amount is ", newAmount);
 
         while (newAmount >= listOfBills[i]) {
             newAmount -= listOfBills[i];
-            console.log("amount after reduced by the value of", listOfBills[i], "is ", newAmount);
-            amountOfBills[i] += 1;
-            console.log("the amountOfBills right now(", listOfBills[i], ") is:", amountOfBills[i]);
-        }
-        console.log("the amount now is < ", newAmount, "and the amountOfBills now is ", amountOfBills);
+            // console.log("amount after reduced by the value of", listOfBills[i], "is ", newAmount, "and value of 500k is ", database["500000"]);
+            newAmountOfBills[i] += 1;
+            // console.log("the newAmountOfBills right now(", listOfBills[i], ") is:", newAmountOfBills[i], "and value of 500k is ", database["500000"]);
+        } 
+        // console.log("the amount now is < ", newAmount, "and the newAmountOfBills now is ", newAmountOfBills);
     }
 
-    console.log("after run the loop, the amnt is ", newAmount, "and the amountOfBills now is ", amountOfBills);
+    // console.log("after run the loop, the amnt is ", newAmount, "and the newAmountOfBills now is ", newAmountOfBills);
     
     //checking if the amnt is in database or not, if not, add it to the database
+    // console.log("before adding value, database is", newDB);
 
     if (!(amnt in listKeysDatabase)) {
-      database[amnt] = amountOfBills;
-      listKeysDatabase.push(parseInt(amnt));
-      listKeysDatabase.sort();
-      //sorting database keys
-      console.log("listKeysDatabase is", listKeysDatabase);
-      
-      let newDatabase = {};
+      database[amnt] = newAmountOfBills;
+    };
+    // console.log("the Database now is", database);
 
-      listKeysDatabase.forEach(function(element){
-        console.log(element);
-        newDatabase[element] = database[element];
-      });
-      database = newDatabase;
-      //This still change the value of 500k bill -> need to work on
-    }
-    console.log("the Database now is", database);
-
-    return amountOfBills;
+    return newAmountOfBills;
 }
 
 function runProgram(){
   let amountEntered = parseInt(document.getElementById("inputform").value);
   let amountOfBills = changeCal(amountEntered);
-  console.log("inside runProgram, the amountOfBills now is", amountOfBills);
-  // let result = document.getElementById("result");
-  amountOfBills.forEach(function(element){
-    console.log(element);
-    let result = document.getElementById("result");
-    result.innerHTML = amountOfBills;
-    //result.innerHTML = ("You will need ", element, "bills of ", listOfBills[(amountOfBills.indexOf(element))]) ;
-  });
+
+  for(let i = 0; i < amountOfBills.length; i++){
+    let id = listOfBills[i];
+    let result = document.getElementById(id);
+    console.log("result is ", result);
+    result.innerHTML = amountOfBills[i];
+  };
+
+  let leftover = document.getElementById("leftover");
+  leftover.innerHTML = newAmount;
 }
